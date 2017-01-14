9c345e618ce1e91ab0d2ed7ec84d1b21909007a2
Christoph Kappestein <k42b3.x@gmail.com>
2017-01-14 10:31:05 +0100

    removed soap request and proxy action and add soap connection

---
+++ src/Connection/Soap.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2016 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Adapter\Soap\Connection;
+
+use Fusio\Engine\ConnectionInterface;
+use Fusio\Engine\Form\BuilderInterface;
+use Fusio\Engine\Form\ElementFactoryInterface;
+use Fusio\Engine\ParametersInterface;
+
+/**
+ * Soap
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class Soap implements ConnectionInterface
+{
+    public function getName()
+    {
+        return 'SOAP';
+    }
+
+    /**
+     * @param \Fusio\Engine\ParametersInterface $config
+     * @return \SoapClient
+     */
+    public function getConnection(ParametersInterface $config)
+    {
+        $options = [];
+
+        $version = $config->get('version');
+        if (!empty($version)) {
+            $options['soap_version'] = $version;
+        }
+
+        $login    = $config->get('username');
+        $password = $config->get('password');
+        if (!empty($login) && !empty($password)) {
+            $options['login']    = $login;
+            $options['password'] = $password;
+        }
+
+        $options['exceptions'] = true;
+
+        $wsdl = $config->get('wsdl');
+        if (empty($wsdl)) {
+            $wsdl = null;
+            $options['location'] = $config->get('location');
+            $options['uri']      = $config->get('uri');
+        }
+
+        return new \SoapClient($wsdl, $options);
+    }
+
+    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory)
+    {
+        $builder->add($elementFactory->newInput('wsdl', 'WSDL', 'text', 'Location of the WSDL specification'));
+        $builder->add($elementFactory->newInput('location', 'Location', 'text', 'Required if no WSDL is available'));
+        $builder->add($elementFactory->newInput('uri', 'Uri', 'text', 'Required if no WSDL is available'));
+        $builder->add($elementFactory->newSelect('version', 'Version', [SOAP_1_1 => '1.1', SOAP_1_2 => '1.2'], 'Optional SOAP version'));
+        $builder->add($elementFactory->newInput('username', 'Username', 'text', 'Optional username for authentication'));
+        $builder->add($elementFactory->newInput('password', 'Password', 'text', 'Optional password for authentication'));
+    }
+}
---
+++ tests/Connection/SoapTest.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Adapter\Soap\Tests\Action;
+
+use Fusio\Adapter\Soap\Connection\Soap;
+use Fusio\Engine\Form\Builder;
+use Fusio\Engine\Form\Container;
+use Fusio\Engine\Form\Element\Input;
+use Fusio\Engine\Form\Element\Select;
+use Fusio\Engine\Parameters;
+use Fusio\Engine\Test\EngineTestCaseTrait;
+
+/**
+ * SoapTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class SoapTest extends \PHPUnit_Framework_TestCase
+{
+    use EngineTestCaseTrait;
+
+    public function testGetConnection()
+    {
+        /** @var Soap $connection */
+        $connection = $this->getConnectionFactory()->factory(Soap::class);
+
+        $config = new Parameters([
+            'location' => 'http://localhost/soap.php',
+            'uri'      => 'http://test-uri/',
+        ]);
+
+        $soap = $connection->getConnection($config);
+
+        $this->assertInstanceOf(\SoapClient::class, $soap);
+    }
+
+    public function testConfigure()
+    {
+        $connection = $this->getConnectionFactory()->factory(Soap::class);
+        $builder    = new Builder();
+        $factory    = $this->getFormElementFactory();
+
+        $connection->configure($builder, $factory);
+
+        $this->assertInstanceOf(Container::class, $builder->getForm());
+
+        $elements = $builder->getForm()->getProperty('element');
+        $this->assertEquals(6, count($elements));
+        $this->assertInstanceOf(Input::class, $elements[0]);
+        $this->assertInstanceOf(Input::class, $elements[1]);
+        $this->assertInstanceOf(Input::class, $elements[2]);
+        $this->assertInstanceOf(Select::class, $elements[3]);
+        $this->assertInstanceOf(Input::class, $elements[4]);
+        $this->assertInstanceOf(Input::class, $elements[5]);
+    }
+}
