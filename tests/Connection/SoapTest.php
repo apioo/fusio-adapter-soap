13b8e4ba83872af4b00dff9f4b6a840d7d4b40f1
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-01-08 17:14:22 +0100

    fix test

--- tests/Connection/SoapTest.php
+++ tests/Connection/SoapTest.php
@@ -66,7 +66,7 @@ class SoapTest extends TestCase
 
         $this->assertInstanceOf(Container::class, $builder->getForm());
 
-        $elements = $builder->getForm()->getProperty('element');
+        $elements = $builder->getForm()->getElements();
         $this->assertEquals(6, count($elements));
         $this->assertInstanceOf(Input::class, $elements[0]);
         $this->assertInstanceOf(Input::class, $elements[1]);
